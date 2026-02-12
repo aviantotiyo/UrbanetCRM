c023b1234d5f8f297c944b066b7efbdd4aa19ce3
aviantotiyo <tiyoavianto@gmail.com>
2026-02-12 20:32:03 +0700

    update typo brand

--- resources/views/partner/auth.blade.php
+++ resources/views/partner/auth.blade.php
@@ -40,7 +40,7 @@
                                     fill="#7367F0" />
                             </svg>
                         </span>
-                        <span class="app-brand-text demo menu-text fw-bold">Urbanet</span>
+                        <span class="app-brand-text demo menu-text fw-bold">Novanet</span>
                     </a>
 
                 </nav>
